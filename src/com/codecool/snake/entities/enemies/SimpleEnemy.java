b2fbc5a11e8e344477388eb92016a08de2d9aff2
alexMillerVince <alex.miller.vince@gmail.com>
2017-09-05 14:07:01 +0200

    add apply

--- src/com/codecool/snake/entities/enemies/SimpleEnemy.java
+++ src/com/codecool/snake/entities/enemies/SimpleEnemy.java
@@ -5,6 +5,7 @@ import com.codecool.snake.Globals;
 import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.Utils;
 import com.codecool.snake.entities.Interactable;
+import com.codecool.snake.entities.laser.Laser;
 import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
@@ -41,9 +42,13 @@ public class SimpleEnemy extends GameEntity implements Animatable, Interactable 
         setY(getY() + heading.getY());
     }
 
-    @Override
     public void apply(SnakeHead player) {
         player.changeHealth(-damage);
+        destroy();
+    }
+
+    @Override
+    public void apply(Laser laser) {
         destroy();
     }
 
