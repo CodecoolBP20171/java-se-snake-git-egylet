124b1cc66fe4d369956cf284cbe9e6bb975f563e
alexMillerVince <alex.miller.vince@gmail.com>
2017-09-05 14:08:51 +0200

    add apply

--- src/com/codecool/snake/entities/powerups/SimplePowerup.java
+++ src/com/codecool/snake/entities/powerups/SimplePowerup.java
@@ -3,6 +3,7 @@ package com.codecool.snake.entities.powerups;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
 import com.codecool.snake.entities.Interactable;
+import com.codecool.snake.entities.laser.Laser;
 import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
@@ -25,6 +26,11 @@ public class SimplePowerup extends GameEntity implements Interactable {
     @Override
     public void apply(SnakeHead snakeHead) {
         snakeHead.addPart(4);
+        destroy();
+    }
+
+    @Override
+    public void apply(Laser laser) {
         destroy();
     }
 
