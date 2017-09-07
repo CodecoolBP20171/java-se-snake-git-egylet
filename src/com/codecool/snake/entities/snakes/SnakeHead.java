9be92d4aba9a28db8071a5e0409058ded2367b46
alexMillerVince <alex.miller.vince@gmail.com>
2017-09-07 14:49:02 +0200

    remove unnecessary imports and getMessage, refactor speed

--- src/com/codecool/snake/entities/snakes/SnakeHead.java
+++ src/com/codecool/snake/entities/snakes/SnakeHead.java
@@ -1,6 +1,5 @@
 package com.codecool.snake.entities.snakes;
 
-import com.codecool.snake.Game;
 import com.codecool.snake.Main;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
@@ -9,15 +8,11 @@ import com.codecool.snake.Utils;
 import com.codecool.snake.entities.Interactable;
 import javafx.geometry.Point2D;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.layout.Pane;
-
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 public class SnakeHead extends GameEntity implements Animatable {
 
-    private static float speed = 2;
+    private float speed;
     private static final float turnRate = 5;
     private GameEntity tail; // the last element. Needed to know where to add the next part.
     private int health;
@@ -29,6 +24,7 @@ public class SnakeHead extends GameEntity implements Animatable {
         setX(xc);
         setY(yc);
         health = 100;
+        speed = 2;
         tail = this;
         setImage(Globals.snakeHead);
         pane.getChildren().add(this);
