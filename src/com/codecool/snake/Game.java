d0d719beddd006b7353d1fa975724e03a4c89172
alexMillerVince <alex.miller.vince@gmail.com>
2017-09-07 11:03:54 +0200

    remove unnecessary import and add second parameter to enemies

--- src/com/codecool/snake/Game.java
+++ src/com/codecool/snake/Game.java
@@ -3,15 +3,12 @@ package com.codecool.snake;
 import com.codecool.snake.entities.enemies.FasterEnemy;
 import com.codecool.snake.entities.enemies.GhostEnemy;
 import com.codecool.snake.entities.enemies.NotSoSimpleEnemy;
-import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.enemies.SimpleEnemy;
 import com.codecool.snake.entities.laser.Laser;
 import com.codecool.snake.entities.powerups.SimplePowerup;
 import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-
-import java.util.Timer;
 
 public class Game extends Pane {
 
@@ -25,14 +22,14 @@ public class Game extends Pane {
         new NotSoSimpleEnemy(this, snakeHead);
         new NotSoSimpleEnemy(this, snakeHead);
 
-        new GhostEnemy(this);
-        new GhostEnemy(this);  
+        new GhostEnemy(this, snakeHead);
+        new GhostEnemy(this, snakeHead);
 
-        new FasterEnemy(this);
-        new FasterEnemy(this);
+        new FasterEnemy(this, snakeHead);
+        new FasterEnemy(this, snakeHead);
 
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
+        new SimpleEnemy(this, snakeHead);
+        new SimpleEnemy(this, snakeHead);
 
         new SimplePowerup(this);
         new SimplePowerup(this);
