44086d49f75f460d3a82d48a6dece3f6635252d0
juhaszTamas <juhasz.tamas991@gmail.com>
2017-09-07 10:37:01 +0200

    health and score label added

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -1,10 +1,16 @@
 package com.codecool.snake;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Label healthHUD = new Label();
+    public static Label scoreHUD = new Label();
 
     public static void main(String[] args) {
         launch(args);
@@ -13,11 +19,26 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Game game = new Game();
-
+        setLabelProperties(game);
         primaryStage.setTitle("Snake Game");
         primaryStage.setScene(new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
         primaryStage.show();
         game.start();
+    }
+
+    public void setLabelProperties(Game game){
+        game.getChildren().add(healthHUD);
+        game.getChildren().add(scoreHUD);
+        scoreHUD.setText("Score: 0");
+        scoreHUD.setFont(new Font("Arial", 25));
+        scoreHUD.setTranslateX(0);
+        scoreHUD.setTranslateY(0);
+        healthHUD.setText("Health: 100");
+        healthHUD.setFont(new Font("Arial", 25));
+        healthHUD.setTranslateX(0);
+        healthHUD.setTranslateY(50);
+        scoreHUD.setStyle("-fx-border-color:lightblue; -fx-background-color:lightblue;");
+        healthHUD.setStyle("-fx-border-color:lightblue; -fx-background-color:lightblue;");
     }
 
 }
