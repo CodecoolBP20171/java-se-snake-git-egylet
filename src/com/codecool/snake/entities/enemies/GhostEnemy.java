c85a3a081206024b1e7058f6dbf4cb43cb177468
alexMillerVince <alex.miller.vince@gmail.com>
2017-09-07 11:11:18 +0200

    remove unnecessary imports, setNewHeading and replace setX, setY by setEnemy

--- src/com/codecool/snake/entities/enemies/GhostEnemy.java
+++ src/com/codecool/snake/entities/enemies/GhostEnemy.java
@@ -1,30 +1,24 @@
 package com.codecool.snake.entities.enemies;
 
 import com.codecool.snake.Globals;
-import com.codecool.snake.Utils;
 import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.Interactable;
 import com.codecool.snake.entities.laser.Laser;
 import com.codecool.snake.entities.snakes.SnakeHead;
-import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
-
-import java.util.Random;
 
 public class GhostEnemy extends GameEntity implements Animatable, Interactable {
 
-    private Point2D heading;
-    private static final int damage = 10;
-    private Random rnd = new Random();
 
-    public GhostEnemy(Pane pane) {
+    private static final int damage = 10;
+
+    public GhostEnemy(Pane pane, SnakeHead player) {
         super(pane);
 
         setImage(Globals.ghostEnemy);
         pane.getChildren().add(this);
-        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
-        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+        setEnemy(player);
         setNewHeading();
 
     }
@@ -59,15 +53,5 @@ public class GhostEnemy extends GameEntity implements Animatable, Interactable {
     @Override
     public String getMessage() {
         return "10 damage";
-    }
-
-    public Point2D setNewHeading(){
-        int speed = 1;
-        double direction = rnd.nextDouble() * 360;
-        System.out.println("Simple: " + direction);
-        setRotate(direction);
-        heading = Utils.directionToVector(direction, speed);
-        System.out.println(heading);
-        return heading;
     }
 }
