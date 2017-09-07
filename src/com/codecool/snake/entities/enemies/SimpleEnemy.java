35cb239a667f44c650ae28f6ce118155003c5a23
alexMillerVince <alex.miller.vince@gmail.com>
2017-09-07 11:10:32 +0200

    remove unnecessary imports, setNewHeading and replace setX, setY by setEnemy

--- src/com/codecool/snake/entities/enemies/SimpleEnemy.java
+++ src/com/codecool/snake/entities/enemies/SimpleEnemy.java
@@ -3,28 +3,21 @@ package com.codecool.snake.entities.enemies;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
 import com.codecool.snake.entities.Animatable;
-import com.codecool.snake.Utils;
 import com.codecool.snake.entities.Interactable;
 import com.codecool.snake.entities.laser.Laser;
 import com.codecool.snake.entities.snakes.SnakeHead;
-import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
-
-import java.util.Random;
 
 public class SimpleEnemy extends GameEntity implements Animatable, Interactable {
 
-    private Point2D heading;
     private static final int damage = 10;
-    private Random rnd = new Random();
 
-    public SimpleEnemy(Pane pane) {
+    public SimpleEnemy(Pane pane, SnakeHead player) {
         super(pane);
 
         setImage(Globals.simpleEnemy);
         pane.getChildren().add(this);
-        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
-        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+        setEnemy(player);
         setNewHeading();
 
     }
@@ -51,16 +44,6 @@ public class SimpleEnemy extends GameEntity implements Animatable, Interactable 
     @Override
     public String getMessage() {
         return "10 damage";
-    }
-
-    public Point2D setNewHeading() {
-        int speed = 1;
-        double direction = rnd.nextDouble() * 360;
-        System.out.println("Simple: " + direction);
-        setRotate(direction);
-        heading = Utils.directionToVector(direction, speed);
-        System.out.println(heading);
-        return heading;
     }
 
     @Override
