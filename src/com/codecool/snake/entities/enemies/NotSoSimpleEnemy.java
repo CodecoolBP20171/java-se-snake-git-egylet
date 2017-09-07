957e17ad9d1866a259c702af5e82c56bbb5c09c0
alexMillerVince <alex.miller.vince@gmail.com>
2017-09-07 11:10:50 +0200

    remove unnecessary imports, setNewHeading and replace setX, setY by setEnemy

--- src/com/codecool/snake/entities/enemies/NotSoSimpleEnemy.java
+++ src/com/codecool/snake/entities/enemies/NotSoSimpleEnemy.java
@@ -3,20 +3,15 @@ package com.codecool.snake.entities.enemies;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
 import com.codecool.snake.entities.Animatable;
-import com.codecool.snake.Utils;
 import com.codecool.snake.entities.Interactable;
 import com.codecool.snake.entities.laser.Laser;
 import com.codecool.snake.entities.snakes.SnakeHead;
-import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
 
-import java.util.Random;
 
 public class NotSoSimpleEnemy extends GameEntity implements Animatable, Interactable {
 
-    private Point2D heading;
     private static final int damage = 10;
-    private Random rnd = new Random();
     private SnakeHead snakeHead;
 
     public NotSoSimpleEnemy(Pane pane, SnakeHead snakeHead) {
@@ -24,8 +19,7 @@ public class NotSoSimpleEnemy extends GameEntity implements Animatable, Interact
         this.snakeHead = snakeHead;
         setImage(Globals.simpleEnemy);
         pane.getChildren().add(this);
-        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
-        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+        setEnemy(snakeHead);
 
     }
 
