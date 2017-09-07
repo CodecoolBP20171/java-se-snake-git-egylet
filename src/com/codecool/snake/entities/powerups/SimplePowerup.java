9435b3f47c5a1599bc6ec500a08aa9f0d7b6ee05
alexMillerVince <alex.miller.vince@gmail.com>
2017-09-07 11:08:21 +0200

    remove unnecessary import

--- src/com/codecool/snake/entities/powerups/SimplePowerup.java
+++ src/com/codecool/snake/entities/powerups/SimplePowerup.java
@@ -5,7 +5,6 @@ import com.codecool.snake.Globals;
 import com.codecool.snake.entities.Interactable;
 import com.codecool.snake.entities.laser.Laser;
 import com.codecool.snake.entities.snakes.SnakeHead;
-import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
 
 import java.util.Random;
